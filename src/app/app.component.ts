9b51e79a7e5a997b8bfaeb70be87e6119752689e
Carlos Ruesta <carlos@dcide.com.br>
2021-07-26 06:45:13 -0500

    aula 03 - Exportanto dados entre componentes. - De um lado (componente origem) criamos uma propriedade do tipo Output ( que leva dados para fora do componente) associada a um emissor de evento que permitirá transferir os dados entre componentes. - Do outro (componente destino) criamos um método que receberá via o evento os dados transferidos. - A vinculação dos dois lados fica registrada no HTML do componente destino que está utilizando o componente origem. Nessa vinculação definimos o evento origem e o método que tratará no destino.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,8 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'angular-byte-bank';
+
+  transferir($event) {
+    console.log($event);
+  }
 }
--- src/app/nova-transferencia/nova-transferencia.component.ts
+++ src/app/nova-transferencia/nova-transferencia.component.ts
@@ -1,4 +1,4 @@
-import {Component} from '@angular/core';
+import {Component, EventEmitter, Output} from '@angular/core';
 
 @Component({
     selector: 'app-nova-transferencia',
@@ -6,10 +6,19 @@ import {Component} from '@angular/core';
     styleUrls: ['./nova-transferencia.component.scss']
 })
 export class NovaTransferenciaComponent {
-  valor: number = 123;
-  destino: number = 222;
+
+  @Output() aoTransferir = new EventEmitter<any>();
+
+  valor: number;
+  destino: number;
+
   transferir() {
     console.log('Solicitada nova transferência');
     console.log(this.valor, this.destino);
+    const dadosTransferencia = {
+      valor: this.valor,
+      destino: this.destino
+    };
+    this.aoTransferir.emit(dadosTransferencia);
   }
 }
