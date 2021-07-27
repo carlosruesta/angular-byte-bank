608dbfff9db4a90605c036287e09ba554548b133
Carlos Ruesta <carlos@dcide.com.br>
2021-07-26 21:05:19 -0300

    aula 04 - Adicionando um novo componente chamado 'extrato'

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,11 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import {NovaTransferenciaComponent} from './nova-transferencia/nova-transferencia.component';
 import {FormsModule} from '@angular/forms';
+import { ExtratoComponent } from './extrato/extrato.component';
 
 @NgModule({
   declarations: [
-    AppComponent, NovaTransferenciaComponent
+    AppComponent, NovaTransferenciaComponent, ExtratoComponent
   ],
   imports: [
     BrowserModule,
